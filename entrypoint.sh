172af2f795aa2a5fb7ee8b65758693c34e7de381
Puneeth Rai <npuneethrai@yahoo.co.in>
2024-02-03 20:37:50 +0530

    Update entrypoint.sh
    to print both to screen and to an output file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo "----------------------------------------------------------------"
-echo "----------------RUN ORIGINAL CUSTOM PYTORCH MODEL---------------"
-echo "----------------------------------------------------------------"
+echo "----------------------------------------------------------------" | tee /mount_folder/result.txt 
+echo "----------------RUN ORIGINAL CUSTOM PYTORCH MODEL---------------" | tee -a /mount_folder/result.txt 
+echo "----------------------------------------------------------------" | tee -a /mount_folder/result.txt 
 
-yolo predict model=/app/orig_pytorch_model/best.pt source=/app/testdata
+yolo predict model=/app/orig_pytorch_model/best.pt source=/app/testdata | tee -a /mount_folder/result.txt 
 
-echo "----------------------------------------------------------------"
-echo "----------------RUN OPENVINO MODEL------------------------------"
-echo "----------------------------------------------------------------"
+echo "----------------------------------------------------------------" | tee -a /mount_folder/result.txt 
+echo "----------------RUN OPENVINO MODEL------------------------------" | tee -a /mount_folder/result.txt 
+echo "----------------------------------------------------------------" | tee -a /mount_folder/result.txt 
 
-yolo predict model=/app/best_openvino_model source=/app/testdata imgsz=224 task=classify
+yolo predict model=/app/best_openvino_model source=/app/testdata imgsz=224 task=classify | tee -a /mount_folder/result.txt 
 
-echo "----------------------------------------------------------------"
-
-
+echo "----------------------------------------------------------------" | tee -a /mount_folder/result.txt 
