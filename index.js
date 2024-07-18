f76a9cd955062c15453e5a0cd8c1f2fabf481a51
Wadhwa, HimanshuX <himanshux.wadhwa@intel.com>
2024-07-18 18:26:59 +0530

    Connect method added

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const mySecretKey = process.env.API_KEY;
+const mySecretKey = 'dtb_user_4cc5f16f46bf4d34a1d952fd2ef647a3' //process.env.API_KEY;
 
 console.log('MY_SECRET_KEY:', mySecretKey);
 
-function connectToDTB(secretKey) {
-    if (secretKey) {
-        console.log(`Connecting to the application with secret key: ${secretKey}`);
-    } else {
-        console.error('No secret key provided. Cannot connect to the application.');
+async function connectToDTB(endpoint) {
+    const url = `${endpoint}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${mySecretKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
 }
 
+
 // use the secret key to connect to the application
-connectToDTB(mySecretKey);
+connectToDTB('https://frontend-staging.apps1.dtb.one-edge.intel.com/');
